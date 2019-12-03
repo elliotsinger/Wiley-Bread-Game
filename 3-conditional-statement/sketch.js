cbebcea765df998ba99734ca49cc28abc26c9e62
Lizzy <lizzy.brooks@gmail.com>
2019-12-03 13:19:33 -0800

    debugged bread issue

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -1,7 +1,6 @@
-//create an empty array called balls
+
 let students = [];
 let slices = [];
-
 //create a variable to hold your avatar
 let me;
 let breadroll;
@@ -13,26 +12,17 @@ function preload(){
 
 function setup() {
   createCanvas(500, 400);
-
   //make one avatar called me
   me = new Avatar(width/2, height/2, 3);
 
   for (let i = 0; i < 300; i++){
       let b = new Bread(random (175,315), random (150,240),false);
       slices.push(b);
-
   }
-
-
 }
-
 
 function draw(){
 	background(220);
-
-
-
-
   strokeWeight(4);
   stroke(51);
   fill(0,0,0, 10);
@@ -40,34 +30,28 @@ function draw(){
 
   for (let i = 0; i < slices.length; i++) {
           slices[i].drawBread();
-
+          slices[i].dropBread();
     }
 
     me.drawMe();
     me.moveMe();
 
   if (frameCount % 100 == 0) {
-      let  b = new Students(width, random(0,height), -3, false);
+      let  b = new Students(width, random(0,height), -3, false, "not yet");
       students.push(b);
-    //  console.log(balls); //print the balls array to the console
     }
 
 if (frameCount % 100 == 0) {
-        let  b = new Students(0, random(0,height), 3, false, false);
-
+        let  b = new Students(0, random(0,height), 3, false, "not yet");
         students.push(b);
-      //  console.log(balls); //print the balls array to the console
     }
 
-//	draw all the balls in that array
 	for (let i = 0; i < students.length; i++) {
 	 	      students[i].drawStudents();
        	  students[i].moveStudents();
         	students[i].loseBread();
           students[i].takeBread();
 	  }
-
-
 
 }
 
@@ -117,58 +101,52 @@ class Avatar {
 }
 
 
-//ball class from which to create new balls with similar properties.
 class Students {
-
-	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed, hitWiley, hasBread){
 		this.x = x;
     this.y = y;
     this.speed = speed;
     this.hitWiley = hitWiley;
     this.hasBread = hasBread;
-
 	}
 
-	// draw a ball on the screen at x,y
 	drawStudents(){
     	stroke(0);
       strokeWeight(1);
     	fill("red");
 		  ellipse(this.x,this.y,10,10);
+      if(this.hasBread == "yes"){
+        image(breadroll, this.x+1, this.y);
+      }
+      if(this.hasBread == "drop it"){
+        print("fall bread!")
+
+      }
 	}
 
-	//update the location of the ball, so it moves across the screen
 	moveStudents(){
 		this.x = this.x+ this.speed;
-		//this.y = this.y+random(.5,5);
     if (this.y <200){
-      //slope = [y+{200-y}]/250);
       this.y = this.y+(200+this.y)/250;
     }
     if (this.y >200){
-    //  slope = [y-{200-y}]/250);
-    this.y = this.y -(200+this.y)/250;
+        this.y = this.y -(200+this.y)/250;
     }
-
 	}
 
 takeBread (){
-  for(let i=0; i<slices.length; i++){
-    if (this.x>= slices [i].x && this.x<= slices[i].x + 10 && this.y <=slices[i].y && this.y <=slices[i].y + 10 && this.hasBread == false){
-      slices[i].follow = true
-      this.hasBread = true
-      print("got a bread")
-    }
+  if(this.x>width/2-50 && this.x<width/2+50 && this.y> height/2-50 && this.y< height/2+50 && this.hasBread == "not yet"){
+    this.hasBread = "yes";
+    slices.pop();
   }
-
 }
 
-	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	loseBread(){
-    		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-30 && this.y < me.y+60 && this.hitWiley == false){
+  loseBread(){
+    	if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-30 && this.y < me.y+60 && this.hitWiley == false){
           this.hitWiley = true
-          this.speed = this.speed;
+          this.speed = -this.speed;
+          print("hit Wiley")
+          this.hasBread = "drop it"
 
     		}
   	}
@@ -186,17 +164,13 @@ drawBread (){
   image(breadroll, this.x, this.y);
 }
 
-moveBread (){
-for(let i=0; i<students.length; i++){
-  if (this.hasBread = true){
-    this.follow = true
-    // this.x = students[i].x
-    // this.y = students[i].y
 
- this.x = 100;
- this.y = 100;
+dropBread(){
+   for(let i=0; i<students.length; i++){
+     if (students[i].hasBread == "yes" && students[i].hitWiley == true){
+       students[i].hasBread == "drop it"
+         }
+       }
+ }
 
-      }
-    }
-  }
 }
